7e879a7a6da527e04e84bad3b109b42789a22918
R.H. Stine <bob@waltonstine.net>
2017-04-17 13:08:04 -0400

    Add Sleep() after write, to avoid a socket-reset error on the server.

--- WebSocketsClient/SendFile.cs
+++ WebSocketsClient/SendFile.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Net.WebSockets;
 using System.Threading;
@@ -8,19 +9,30 @@ namespace waltonstine.websockets.demo
 {
     public class SendFile
     {
-        private async static Task UploadFile(string fname) {
+        private async static Task UploadFile(Stream strm) {
 
-            HttpResponseMessage response = null;
+            byte[]            bytes = new byte[1048576];
+            BinaryReader      br   = new BinaryReader(strm);
+
+            int cnt = br.Read(bytes, 0, bytes.Length);
+
+            ArraySegment<byte> buff = new ArraySegment<byte>(bytes);
 
             ClientWebSocket   sock = new ClientWebSocket();
             CancellationToken tok  = new CancellationToken();
             Uri serverUri          = new Uri("ws://localhost:5000/upload");
+
             Task sockTask          = sock.ConnectAsync(serverUri, tok);
             if ( ! sockTask.Wait(5000) ) {
                 Console.WriteLine("Timeout attempting WebSocket connection");
                 return;
             }
             Console.WriteLine("CONNECTED!!! <<<<<<<");
+
+            await sock.SendAsync(buff, WebSocketMessageType.Binary, true, tok);
+            Console.WriteLine("Bytes sent! <<<<<<<");
+
+            Thread.Sleep(2000);
 
             await sock.CloseAsync(WebSocketCloseStatus.NormalClosure, "all she wrote", tok);
 
@@ -37,7 +49,9 @@ namespace waltonstine.websockets.demo
                 return;
             }
 
-            Task uploadTask = UploadFile(args[0]);
+            FileStream fs = new FileStream(args[0], FileMode.Open, FileAccess.Read);
+
+            Task uploadTask = UploadFile(fs);
 
             uploadTask.Wait();
 
